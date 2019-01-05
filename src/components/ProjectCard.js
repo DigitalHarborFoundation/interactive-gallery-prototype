be861633ecaaecca587bab1f1a350f2a4adaed1a
jonathanprozzi <jonathanprozzi@gmail.com>
2019-01-04 19:12:22 -0500

    modified the box shadow for the ProjectCard component

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -30,10 +30,8 @@ const CardWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   background-color: #fff;
-  /* padding: 2vh 2vw; */
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2);
   border-radius: 10px;
-  /* transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1); */
   transition: all 0.5s ease-in-out;
 
   :hover {
