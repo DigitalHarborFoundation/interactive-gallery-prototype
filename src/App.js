94e8caa1b39c68cca06b0addf93f7ee62e1d503b
jonathanprozzi <jonathanprozzi@gmail.com>
2019-01-08 17:26:47 -0500

    moved Portal into ProjectModal so that the App can just use the ProjectModal

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ProjectCardGrid from "./components/ProjectCardGrid";
-import Portal from "./components/Portal";
+import ProjectModal from "./components/ProjectModal";
 import { entries } from "./data";
 
 class App extends Component {
@@ -28,12 +28,12 @@ class App extends Component {
   render() {
     return (
       <AppWrapper>
+        <ProjectModal />
         <Header selectCourse={this.selectCourse.bind(this)} />
         <ProjectCardGrid
           entries={this.state.entries}
           selectedCourse={this.state.selectedCourse}
         />
-        <Portal />
         <Footer />
       </AppWrapper>
     );
--- src/components/ProjectModal.js
+++ src/components/ProjectModal.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Portal from "./Portal";
 
 class ProjectModal extends Component {
   render() {
-    return <div />;
+    return (
+      <Portal>
+        <ModalWrapper>{this.props.children}</ModalWrapper>
+      </Portal>
+    );
   }
 }
 
 export default ProjectModal;
+
+const ModalWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: papayawhip;
+`;
