3e3832de5b7e72fc63ae738537f0cca7360ba71f
jonathanprozzi <jonathanprozzi@gmail.com>
2019-01-08 17:16:40 -0500

    testing Portal in App component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ProjectCardGrid from "./components/ProjectCardGrid";
+import Portal from "./components/Portal";
 import { entries } from "./data";
 
 class App extends Component {
@@ -32,6 +33,9 @@ class App extends Component {
           entries={this.state.entries}
           selectedCourse={this.state.selectedCourse}
         />
+        <Portal>
+          <h2>Testing Portal</h2>
+        </Portal>
         <Footer />
       </AppWrapper>
     );
