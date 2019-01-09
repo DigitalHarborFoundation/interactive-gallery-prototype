1e440c62a374960f19c343fd37ce5a0b73fe0deb
jonathanprozzi <jonathanprozzi@gmail.com>
2019-01-08 22:30:34 -0500

    added toggle function and button styles to the Toggle component

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,8 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
 
-const Toggle = () => (
+class Toggle extends Component {
+  state = {
+    on: false
+  };
 
-)
+  toggle = () => {
+    this.setState({
+      on: !this.state.on
+    });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.on && <h1>Toggle Tester</h1>}
+        <ToggleButton onClick={this.toggle} />
+      </React.Fragment>
+    );
+  }
+}
 
 export default Toggle;
 
+const ToggleButton = styled.button`
+  width: 24rem;
+  border: 2px solid #fafafa;
+  border-radius: 15px;
+  background-color: rgba(196, 196, 196, 0);
+  color: #fafafa;
+  text-transform: uppercase;
+  font-size: 2rem;
+  font-weight: 300;
+  padding: 1.6rem 2.4rem;
+  text-align: center;
+  transition: all ease 0.5s;
+
+  &:hover {
+    border: 2px solid #0091c9;
+    color: #0091c9;
+    cursor: pointer;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  }
+
+  &:focus {
+    outline: 0;
+  }
+`;
