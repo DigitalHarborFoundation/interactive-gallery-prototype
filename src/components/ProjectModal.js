70ca04456f392c74374a05a5f7f8eebbf15ca160
jonathanprozzi <jonathanprozzi@gmail.com>
2019-01-10 14:48:58 -0500

    added temporary bg color (papayawhip) to the card photos to debug the height not being 100% for all photos

--- src/components/ProjectModal.js
+++ src/components/ProjectModal.js
@@ -82,7 +82,7 @@ const ModalWrapper = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background: rgba(0, 0, 0, 0.7);
+  background: rgba(0, 0, 0, 0.85);
   transition: background ease-in-out 1s;
 `;
 
