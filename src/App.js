c8ec39129ba5dea9392cb434f0962dc96ab45a1a
jonathanprozzi <jonathanprozzi@gmail.com>
2019-01-11 15:22:18 -0500

    moved Header component directly into App component to add additional elements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import { Parallax, ParallaxLayer } from "react-spring/addons";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ProjectCardGrid from "./components/ProjectCardGrid";
 import ProjectModal from "./components/ProjectModal";
 import Toggle from "./components/Toggle";
 import { entries } from "./data";
+import heroImg from "./hero-img.jpg";
 
 class App extends Component {
   state = {
@@ -27,7 +27,24 @@ class App extends Component {
   render() {
     return (
       <AppWrapper>
-        <Header selectCourse={this.selectCourse.bind(this)} />
+        <Masthead>
+          <LogoWrapper>
+            <DHFTitle>Digital Harbor Foundation</DHFTitle>
+          </LogoWrapper>
+          <CTAContainer>
+            <ProjectTitle>Interactive Project Gallery</ProjectTitle>
+            <ButtonContainer>
+              <GhostButton onClick={this.selectCourse.bind(this, "VectorFab")}>
+                VectorFab
+              </GhostButton>
+              <GhostButton
+                onClick={this.selectCourse.bind(this, "Game Workshop")}
+              >
+                Game Workshop
+              </GhostButton>
+            </ButtonContainer>
+          </CTAContainer>
+        </Masthead>
         <Toggle>
           {({ on, toggle }) => (
             <React.Fragment>
@@ -54,4 +71,90 @@ export default App;
 
 const AppWrapper = styled.div`
   min-height: 100vh;
+`;
+
+const LogoWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  background: rgba(45, 42, 42, 0.8);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  width: 100%;
+`;
+
+const DHFTitle = styled.h1`
+  font-size: 4.8rem;
+  font-weight: 300;
+  color: #fafafa;
+`;
+
+const Masthead = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 80vh;
+  min-width: 100vw;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 145, 201, 0.3), rgba(0, 145, 201, 0.3)),
+    url(${heroImg});
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const CTAContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 16vh;
+`;
+
+const ProjectTitle = styled.h2`
+  font-size: 4rem;
+  font-weight: normal;
+  color: #fafafa;
+  margin-bottom: 10vh;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-items: center;
+  align-items: center;
+`;
+
+const GhostButton = styled.button`
+  width: 24rem;
+  border: 2px solid #fafafa;
+  border-radius: 15px;
+  background-color: rgba(196, 196, 196, 0);
+  color: #fafafa;
+  text-transform: uppercase;
+  font-size: 2rem;
+  font-weight: 400;
+  letter-spacing: 0.05rem;
+  padding: 1.6rem 2.4rem;
+  text-align: center;
+  transition: all ease 0.5s;
+
+  &:first-child {
+    margin-right: 2.4rem;
+  }
+
+  &:last-child {
+    margin-left: 2.4rem;
+  }
+
+  &:hover {
+    border: 2px solid #0091c9;
+    color: #0091c9;
+    cursor: pointer;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  }
+
+  &:focus {
+    outline: 0;
+  }
 `;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,12 @@
 import React from "react";
 import styled from "styled-components";
-import { Parallax, ParallaxLayer } from "react-spring/addons";
-import heroImg from "../hero-img.jpg";
 
 const Header = props => (
-  <HeaderWrapper>
-    <LogoWrapper>
-      <DHFTitle>Digital Harbor Foundation</DHFTitle>
-    </LogoWrapper>
-    <CTAContainer>
-      <ProjectTitle>Interactive Project Gallery</ProjectTitle>
-      <ButtonContainer>
-        <GhostButton onClick={props.selectCourse.bind(this, "VectorFab")}>
-          VectorFab
-        </GhostButton>
-        <GhostButton onClick={props.selectCourse.bind(this, "Game Workshop")}>
-          Game Workshop
-        </GhostButton>
-      </ButtonContainer>
-    </CTAContainer>
-  </HeaderWrapper>
+  <LogoWrapper>
+    <DHFTitle>Digital Harbor Foundation</DHFTitle>
+  </LogoWrapper>
 );
 export default Header;
-
-const HeaderWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  min-height: 80vh;
-  min-width: 100vw;
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(rgba(0, 145, 201, 0.3), rgba(0, 145, 201, 0.3)),
-    url(${heroImg});
-  background-repeat: no-repeat;
-  background-size: cover;
-`;
 
 const LogoWrapper = styled.div`
   display: flex;
@@ -51,60 +22,4 @@ const DHFTitle = styled.h1`
   font-size: 4.8rem;
   font-weight: 300;
   color: #fafafa;
-`;
-
-const CTAContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin-top: 16vh;
-`;
-
-const ProjectTitle = styled.h2`
-  font-size: 4rem;
-  font-weight: normal;
-  color: #fafafa;
-  margin-bottom: 10vh;
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-items: center;
-  align-items: center;
-`;
-
-const GhostButton = styled.button`
-  width: 24rem;
-  border: 2px solid #fafafa;
-  border-radius: 15px;
-  background-color: rgba(196, 196, 196, 0);
-  color: #fafafa;
-  text-transform: uppercase;
-  font-size: 2rem;
-  font-weight: 400;
-  letter-spacing: 0.05rem;
-  padding: 1.6rem 2.4rem;
-  text-align: center;
-  transition: all ease 0.5s;
-
-  &:first-child {
-    margin-right: 2.4rem;
-  }
-
-  &:last-child {
-    margin-left: 2.4rem;
-  }
-
-  &:hover {
-    border: 2px solid #0091c9;
-    color: #0091c9;
-    cursor: pointer;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  }
-
-  &:focus {
-    outline: 0;
-  }
 `;
