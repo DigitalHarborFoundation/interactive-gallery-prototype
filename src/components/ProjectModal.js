d435c77947b4c5bc97f80b9fe19111eafdc0506e
jonathanprozzi <jonathanprozzi@gmail.com>
2019-01-10 23:37:01 -0500

    put Transition on hold at the moment -- need to experiment

--- src/components/ProjectModal.js
+++ src/components/ProjectModal.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+
 import Portal from "./Portal";
 import Separator from "./Separator";
 import Icon from "./Icon";
