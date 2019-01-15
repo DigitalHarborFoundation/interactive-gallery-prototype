a1152952a6993a232a5a863081184325933c1260
jonathanprozzi <jonathanprozzi@gmail.com>
2019-01-15 13:09:00 -0500

    moved original test data into mockData.js and began entering real data from the youth into data.js

--- src/data.js
+++ src/data.js
@@ -1,41 +1,82 @@
 export const entries = [
   {
     id: 1,
-    youthName: "Jonathan",
+    youthName: "Alexandra",
     projectSemester: "Fall 2018",
     enrolledCourse: "VectorFab",
-    projectTitle: "My French Press",
+    projectTitle: "Handheld Maze",
     makerStatement:
-      "A Organic latte, sugar grounds, medium extra con panna as turkish. Coffee id flavour mazagran cortado foam, aroma caffeine brewed caramelization froth americano. ",
+      "I used a more organic, slightly mind-bending style. [In Inkscape], I started with one line and drew more until I liked the way it looked. ",
     imgSource: "/images/tester-typewriter-card.jpg"
   },
   {
     id: 2,
-    youthName: "Shawn",
+    youthName: "Noah",
     projectSemester: "Fall 2018",
-    enrolledCourse: "Game Workshop",
-    projectTitle: "My French Press",
+    enrolledCourse: "VectorFab",
+    projectTitle: "Otrio Board",
     makerStatement:
-      "A Organic latte, sugar grounds, medium extra con panna as turkish. Coffee id flavour mazagran cortado foam, aroma caffeine brewed caramelization froth americano. Mocha, pumpkin spice, half and half, redeye coffee caffeine in caffeine froth.prototype is a first model or version of something",
+      "I wanted to make a travel sized Otrio Board. I had trouble figuring out how to engrave the way I wanted it to look. ",
     imgSource: "/images/tester-typewriter-card.jpg"
   },
   {
     id: 3,
-    youthName: "Steph",
+    youthName: "Ashlynn",
     projectSemester: "Fall 2018",
     enrolledCourse: "VectorFab",
-    projectTitle: "My French Press",
+    projectTitle: "Jigsaw Puzzle",
     makerStatement:
-      "A Organic latte, sugar grounds, medium extra con panna as turkish. Coffee id flavour mazagran cortado foam, aroma caffeine brewed caramelization froth americano. Mocha, pumpkin spice, half and half, redeye coffee caffeine in caffeine froth.prototype is a first model or version of something",
-    imgSource: "/images/tester-card.jpg"
+      "I made this design on Inkscape. I painted the plywood before I cut my design with the laser cutter.",
+    imgSource: "/images/tester-typewriter-card.jpg"
   },
   {
     id: 4,
-    youthName: "Andrew",
+    youthName: "Jeshua",
     projectSemester: "Fall 2018",
     enrolledCourse: "VectorFab",
-    projectTitle: "My French Press",
+    projectTitle: "Animator Clash",
     makerStatement:
-      "A Organic latte, sugar grounds, medium extra con panna as turkish. Coffee id flavour mazagran cortado foam, aroma caffeine brewed caramelization froth americano. Mocha, pumpkin spice, half and half, redeye coffee caffeine in caffeine froth.prototype is a first model or version of something"
+      "I themed this game around youtube animators and art. It's meant for entertainment and practicing art in short periods of time. ",
+    imgSource: "/images/tester-typewriter-card.jpg"
+  },
+  {
+    id: 5,
+    youthName: "Bella",
+    projectSemester: "Fall 2018",
+    enrolledCourse: "VectorFab",
+    projectTitle: "Platonic Solid",
+    makerStatement:
+      "I wanted to create a 3D box out of 2D shapes. I used images of things that say something about myself.",
+    imgSource: "/images/tester-typewriter-card.jpg"
+  },
+  {
+    id: 6,
+    youthName: "Gabrielle",
+    projectSemester: "Fall 2018",
+    enrolledCourse: "VectorFab",
+    projectTitle: "Handheld Maze",
+    makerStatement:
+      "I inspired my own design. I drew out parts of the maze with a paper and pencil before I created it in Inkscape.",
+    imgSource: "/images/tester-typewriter-card.jpg"
+  },
+  {
+    id: 7,
+    youthName: "Ezekiel",
+    projectSemester: "Fall 2018",
+    enrolledCourse: "VectorFab",
+    projectTitle: "Self-Portrait",
+    makerStatement:
+      "This is a picture of myself I created in Inkscape when I was trying to figure out how to use it. I drew a lacrosse stick because I play lacrosse.",
+    imgSource: "/images/tester-typewriter-card.jpg"
+  },
+  {
+    id: 8,
+    youthName: "Joshua",
+    projectSemester: "Fall 2018",
+    enrolledCourse: "VectorFab",
+    projectTitle: "Handheld Maze",
+    makerStatement:
+      "I wanted to try to make some kind of Mario maze. This maze is for entertainment. I would like to keep making iterations on it. ",
+    imgSource: "/images/tester-typewriter-card.jpg"
   }
 ];
---
+++ src/mockData.js
@@ -0,0 +1,41 @@
+export const entries = [
+  {
+    id: 1,
+    youthName: "Jonathan",
+    projectSemester: "Fall 2018",
+    enrolledCourse: "VectorFab",
+    projectTitle: "My French Press",
+    makerStatement:
+      "A Organic latte, sugar grounds, medium extra con panna as turkish. Coffee id flavour mazagran cortado foam, aroma caffeine brewed caramelization froth americano. ",
+    imgSource: "/images/tester-typewriter-card.jpg"
+  },
+  {
+    id: 2,
+    youthName: "Shawn",
+    projectSemester: "Fall 2018",
+    enrolledCourse: "Game Workshop",
+    projectTitle: "My French Press",
+    makerStatement:
+      "A Organic latte, sugar grounds, medium extra con panna as turkish. Coffee id flavour mazagran cortado foam, aroma caffeine brewed caramelization froth americano. Mocha, pumpkin spice, half and half, redeye coffee caffeine in caffeine froth.prototype is a first model or version of something",
+    imgSource: "/images/tester-typewriter-card.jpg"
+  },
+  {
+    id: 3,
+    youthName: "Steph",
+    projectSemester: "Fall 2018",
+    enrolledCourse: "VectorFab",
+    projectTitle: "My French Press",
+    makerStatement:
+      "A Organic latte, sugar grounds, medium extra con panna as turkish. Coffee id flavour mazagran cortado foam, aroma caffeine brewed caramelization froth americano. Mocha, pumpkin spice, half and half, redeye coffee caffeine in caffeine froth.prototype is a first model or version of something",
+    imgSource: "/images/tester-card.jpg"
+  },
+  {
+    id: 4,
+    youthName: "Andrew",
+    projectSemester: "Fall 2018",
+    enrolledCourse: "VectorFab",
+    projectTitle: "My French Press",
+    makerStatement:
+      "A Organic latte, sugar grounds, medium extra con panna as turkish. Coffee id flavour mazagran cortado foam, aroma caffeine brewed caramelization froth americano. Mocha, pumpkin spice, half and half, redeye coffee caffeine in caffeine froth.prototype is a first model or version of something"
+  }
+];
