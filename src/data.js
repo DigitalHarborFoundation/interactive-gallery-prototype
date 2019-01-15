3bddae7852f4ae6cb0f09de5b00d2380b1cf3724
jonathanprozzi <jonathanprozzi@gmail.com>
2019-01-15 14:42:13 -0500

    photos added for all youth and linked for vectorfab

--- src/data.js
+++ src/data.js
@@ -7,7 +7,7 @@ export const entries = [
     projectTitle: "Handheld Maze",
     makerStatement:
       "I used a more organic, slightly mind-bending style. [In Inkscape], I started with one line and drew more until I liked the way it looked. ",
-    imgSource: "/images/tester-typewriter-card.jpg"
+    imgSource: "/images/alexandra-handheldmaze.jpg"
   },
   {
     id: 2,
@@ -17,7 +17,7 @@ export const entries = [
     projectTitle: "Otrio Board",
     makerStatement:
       "I wanted to make a travel sized Otrio Board. I had trouble figuring out how to engrave the way I wanted it to look. ",
-    imgSource: "/images/tester-typewriter-card.jpg"
+    imgSource: "/images/noah-otrioboard.jpg"
   },
   {
     id: 3,
@@ -27,7 +27,7 @@ export const entries = [
     projectTitle: "Jigsaw Puzzle",
     makerStatement:
       "I made this design on Inkscape. I painted the plywood before I cut my design with the laser cutter.",
-    imgSource: "/images/tester-typewriter-card.jpg"
+    imgSource: "/images/ashlynn-finalproject.jpg"
   },
   {
     id: 4,
@@ -37,7 +37,7 @@ export const entries = [
     projectTitle: "Animator Clash",
     makerStatement:
       "I themed this game around youtube animators and art. It's meant for entertainment and practicing art in short periods of time. ",
-    imgSource: "/images/tester-typewriter-card.jpg"
+    imgSource: "/images/jeshua-finalproject-vf.jpg"
   },
   {
     id: 5,
@@ -47,7 +47,7 @@ export const entries = [
     projectTitle: "Platonic Solid",
     makerStatement:
       "I wanted to create a 3D box out of 2D shapes. I used images of things that say something about myself.",
-    imgSource: "/images/tester-typewriter-card.jpg"
+    imgSource: "/images/bella-platonicsolid.jpg"
   },
   {
     id: 6,
@@ -57,7 +57,7 @@ export const entries = [
     projectTitle: "Handheld Maze",
     makerStatement:
       "I inspired my own design. I drew out parts of the maze with a paper and pencil before I created it in Inkscape.",
-    imgSource: "/images/tester-typewriter-card.jpg"
+    imgSource: "/images/gabrielle-handheldmaze.jpg"
   },
   {
     id: 7,
@@ -67,7 +67,7 @@ export const entries = [
     projectTitle: "Self-Portrait",
     makerStatement:
       "This is a picture of myself I created in Inkscape when I was trying to figure out how to use it. I drew a lacrosse stick because I play lacrosse.",
-    imgSource: "/images/tester-typewriter-card.jpg"
+    imgSource: "/images/ezekiel-selfportrait.jpg"
   },
   {
     id: 8,
@@ -77,7 +77,7 @@ export const entries = [
     projectTitle: "Handheld Maze",
     makerStatement:
       "I wanted to try to make some kind of Mario maze. This maze is for entertainment. I would like to keep making iterations on it. ",
-    imgSource: "/images/tester-typewriter-card.jpg"
+    imgSource: "/images/joshua-handheldmaze.jpg"
   },
   {
     id: 9,
@@ -147,6 +147,16 @@ export const entries = [
     projectTitle: "Pong Pong Snake",
     makerStatement:
       "Link to My Game: https://editor.p5js.org/beepbeepimmasheep2/sketches/B1PEW0B1N",
+    imgSource: "/images/tester-typewriter-card.jpg"
+  },
+  {
+    id: 16,
+    youthName: "Aliyah",
+    projectSemester: "Fall 2018",
+    enrolledCourse: "Game Workshop",
+    projectTitle: "Unpasteurized Honey",
+    makerStatement:
+      "Link to My Game: https://editor.p5js.org/im.threw/sketches/B1ePSVulV",
     imgSource: "/images/tester-typewriter-card.jpg"
   }
 ];
