8c1c9b53d3b87d78447a39e3e048d7930d966195
jonathanprozzi <jonathanprozzi@gmail.com>
2019-01-17 13:12:21 -0500

    styled MemberCourses components

--- src/components/MemberCourses.js
+++ src/components/MemberCourses.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import csCoreImg from "./mb-cscore-img.jpg";
+import fabCoreImg from "./mb-fabcore-img.jpg";
 
 const MemberCourses = () => (
   <ContentWrapper>
@@ -7,33 +9,45 @@ const MemberCourses = () => (
     <MainGrid>
       <CardWrapper>
         <ProjectImageContainer>
-          <ProjectImage />
+          <ProjectImage src={csCoreImg} />
         </ProjectImageContainer>
-        <ProjectTitle>CS Core</ProjectTitle>
-        <YouthName>
+        <CourseTitle>CS Core</CourseTitle>
+        <CourseDescription>
           CS Core is an introduction to the fundamentals of programming. This
           course is focused on building a solid foundation through projects and
-          examples of best practices. Youth will learn programming skills
-          through modern JavaScript and the p5.js library. There is a focus on
-          creating interactive web experiences and core skills that will lead
-          into future computer science courses. This course is the first in our
-          Computer Science Pathway and is a prerequisite to following courses.
-        </YouthName>
+          examples of best practices.
+          <br />
+          <br />
+          Youth will learn programming skills through modern JavaScript and the
+          p5.js library. There is a focus on creating interactive web
+          experiences and core skills that will lead into future computer
+          science courses.
+          <br />
+          <br />
+          This course is the first in our Computer Science Pathway and is a
+          prerequisite to following courses.
+        </CourseDescription>
       </CardWrapper>
       <CardWrapper>
         <ProjectImageContainer>
-          <ProjectImage />
+          <ProjectImage src={fabCoreImg} />
         </ProjectImageContainer>
-        <ProjectTitle>Fab Core</ProjectTitle>
-        <YouthName>
+        <CourseTitle>Fab Core</CourseTitle>
+        <CourseDescription>
           CS Core is an introduction to the fundamentals of programming. This
           course is focused on building a solid foundation through projects and
-          examples of best practices. Youth will learn programming skills
-          through modern JavaScript and the p5.js library. There is a focus on
-          creating interactive web experiences and core skills that will lead
-          into future computer science courses. This course is the first in our
-          Computer Science Pathway and is a prerequisite to following courses.
-        </YouthName>
+          examples of best practices.
+          <br />
+          <br />
+          Youth will learn programming skills through modern JavaScript and the
+          p5.js library. There is a focus on creating interactive web
+          experiences and core skills that will lead into future computer
+          science courses.
+          <br />
+          <br />
+          This course is the first in our Computer Science Pathway and is a
+          prerequisite to following courses.
+        </CourseDescription>
       </CardWrapper>
     </MainGrid>
   </ContentWrapper>
@@ -56,8 +70,8 @@ const SectionTitle = styled.h2`
 const MainGrid = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
-  grid-column-gap: 2vw;
-  grid-row-gap: 10vh;
+  grid-column-gap: 4vw;
+  grid-row-gap: 2vh
   margin-top: 5rem;
   @media only screen and (max-width: 1000px) {
     grid-template-columns: 1fr;
@@ -100,16 +114,54 @@ const ProjectInfoContainer = styled.div`
   margin: 4rem 0;
 `;
 
-const YouthName = styled.p`
-  color: #000;
-  font-size: 3.2rem;
-  margin: 0;
-  padding: 0;
-`;
-
-const ProjectTitle = styled.h3`
+const CourseTitle = styled.h3`
   color: #000;
   font-size: 3.2rem;
   margin-bottom: 0;
   padding-bottom: 0;
+`;
+
+const CourseDescription = styled.p`
+  color: #000;
+  font-size: 2.4rem;
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+const GhostButton = styled.button`
+  width: fit-content;
+  border: 2px solid #0091c9;
+  border-radius: 15px;
+  background-color: rgba(196, 196, 196, 0);
+  color: #0091c9;
+  text-transform: uppercase;
+  font-size: 2rem;
+  font-weight: 400;
+  letter-spacing: 0.05rem;
+  padding: 1.6rem 2.4rem;
+  text-align: center;
+  transition: all ease 0.5s;
+
+  &:first-child {
+    margin-right: 2.4rem;
+  }
+
+  &:last-child {
+    margin-left: 2.4rem;
+  }
+
+  &:hover {
+    border: 2px solid #0091c9;
+    color: #0091c9;
+    cursor: pointer;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  }
+
+  &:focus {
+    outline: 0;
+  }
+
+  @media only screen and (max-width: 1000px) {
+    width: 100%;
+  }
 `;
